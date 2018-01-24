cf8f6bdc7d91c0c8f466a0dfe259304e760afcc7
JounQin <admin@1stg.me>
2018-01-24 11:27:01 +0800

    Allow manual injection with `manualInject` option (#13)
    * judge `isVue` from loader options alose
    
    * generate id via request + resourcePath
    
    * fix issue about null options
    
    * Update index.js
    
    * Update index.js

--- index.js
+++ index.js
@@ -18,12 +18,13 @@ module.exports.pitch = function (remainingRequest) {
   var addStylesServerPath = loaderUtils.stringifyRequest(this, '!' + path.join(__dirname, 'lib/addStylesServer.js'))
 
   var request = loaderUtils.stringifyRequest(this, '!!' + remainingRequest)
-  var id = JSON.stringify(hash(request))
+  var id = JSON.stringify(hash(request + this.resourcePath))
+  var options = loaderUtils.getOptions(this) || {}
 
-  // direct css import from js --> direct (how does this work when inside an async chunk? ...just don't do it)
+  // direct css import from js --> direct, or manually call `styles.__inject__(ssrContext)` with `manualInject` option
   // css import from vue file --> component lifecycle linked
   // style embedded in vue file --> component lifecycle linked
-  var isVue = /"vue":true/.test(remainingRequest)
+  var isVue = /"vue":true/.test(remainingRequest) || options.manualInject
 
   var shared = [
     '// style-loader: Adds some css to the DOM by adding a <style> tag',
